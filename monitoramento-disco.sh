8e280560b31151a2d2fe2c070b576d310f408e88
Gabrielle Ribeiro <33001620+Gabrielle-Ribeiro@users.noreply.github.com>
2024-03-14 17:16:53 -0300

    Delete monitoramento-disco.sh

--- monitoramento-disco.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ponto_montagem="/mnt/c"
-nome_log=$(date +%F-%H:%M)
-diretorio="/home/gabi"
-
-uso_disco=$(df -h | grep $ponto_montagem | awk '{print $5}')
-echo "Uso de disco em $uso_disco" > "$diretorio/$nome_log.log"
